a1ac1596789203b9c3f8adc0cc1d16897619a3f8
Bonfire <bonfire.hf@gmail.com>
2019-12-20 04:47:41 -0500

    Replace interface checking for PVP switch interface with help from "catch (Throwable e) {}"

--- bonScouter/src/com/bonfire/Hop.java
+++ bonScouter/src/com/bonfire/Hop.java
@@ -2,15 +2,24 @@ package com.bonfire;
 
 import org.rspeer.runetek.adapter.component.InterfaceComponent;
 import org.rspeer.runetek.api.Game;
+import org.rspeer.runetek.api.Worlds;
 import org.rspeer.runetek.api.commons.math.Random;
+import org.rspeer.runetek.api.component.InterfaceAddress;
+import org.rspeer.runetek.api.component.InterfaceComposite;
 import org.rspeer.runetek.api.component.Interfaces;
 import org.rspeer.runetek.api.component.WorldHopper;
 import org.rspeer.script.task.Task;
 import org.rspeer.ui.Log;
 
 public class Hop extends Task {
+    public static final InterfaceAddress SWITCH_TO_IT_BUTTON_ADDRESS = new InterfaceAddress(() -> {
+        return Interfaces.getFirst(InterfaceComposite.ITEM_DIALOG.getGroup(), 0, a -> a.containsAction("Switch to it"));
+    });
+
     @Override
     public boolean validate() {
+        // Here we do a null-check for isUseWebhook to make sure the user has saved their settings in the GUI
+        // isUseWebhook can be true or false, but is only null when the user did not click the save button in the GUI
         return bonScouter.isScanComplete() && bonScouter.isUseWebhook() != null && Game.isLoggedIn();
     }
 
@@ -19,18 +28,23 @@ public class Hop extends Task {
         // Hop worlds and increment the world hop count
         Log.fine("Hopping worlds...");
 
+        boolean P2PCheck = scouterGUI.getMembersCheckBox();
+        boolean WildyCheck = scouterGUI.getWildernessCheckBox();
+        boolean PVPCheck = scouterGUI.getpvpCheckBox();
+
         // Get world filters from user's input and hop to the worlds filtered by the predicate
         // TODO: FIX HOPPING WITH MULTIPLE FILTERS ACTIVE
-        WorldHopper.randomHop(world -> world.isMembers() == scouterGUI.getMembersCheckBox()
-                        && world.getActivity().contains("Wilderness") == scouterGUI.getWildernessCheckBox()
-                        && world.isPVP() == scouterGUI.getpvpCheckBox());
+        WorldHopper.randomHop(rsWorld
+                -> rsWorld.getId() != Worlds.getCurrent()
+                && rsWorld.isMembers() == P2PCheck
+                && rsWorld.isPVP() == PVPCheck
+                && rsWorld.getActivity().contains("PK") == WildyCheck);
 
         // Make sure to check if we need to confirm world hopping to PVP worlds
         // Note: This may cause the script to hang for one world hop as the interface does not show until
         //          choose to hop to the world (+1 count to hop, +1 count to confirm and actually hop)
-        InterfaceComponent pvpSwitchInterface = Interfaces.getComponent(193, 0, 3);
-        if (pvpSwitchInterface != null && pvpSwitchInterface.isVisible()){
-            pvpSwitchInterface.interact("Switch to it");
+        if (SWITCH_TO_IT_BUTTON_ADDRESS.resolve() != null){
+            SWITCH_TO_IT_BUTTON_ADDRESS.resolve().click();
         }
 
         // Increment the world count
