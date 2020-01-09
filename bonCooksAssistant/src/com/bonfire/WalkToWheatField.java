719641db359ea6d7e883a041da0084f9e5cbe053
Bonfire <bonfire.hf@gmail.com>
2020-01-09 02:10:57 -0500

    Add walking to wheat field task

--- bonCooksAssistant/src/com/bonfire/WalkToWheatField.java
+++ bonCooksAssistant/src/com/bonfire/WalkToWheatField.java
@@ -9,22 +9,23 @@ import org.rspeer.runetek.api.scene.Players;
 import org.rspeer.script.task.Task;
 import org.rspeer.ui.Log;
 
-public class WalkToChickens extends Task {
+public class WalkToWheatField extends Task {
     @Override
     public boolean validate() {
-        // If we have a bucket of milk and no eggs, it's time to go grab an egg...
+        // If we have a bucket of milk and an egg, but no flour, and we're not in the wheat field
         Player localPlayer = Players.getLocal();
 
         return Varps.get(bonCooksAssistant.cooksAssistantVarp) == 1
                 && Inventory.getCount(false, "Bucket of milk") >= 1
-                && Inventory.getCount(false, "Egg") < 1
-                && !bonCooksAssistant.lumbridgeChickens.contains(localPlayer.getPosition());
+                && Inventory.getCount(false, "Egg") >= 1
+                && Inventory.getCount(false, "Wheat") < 1
+                && !bonCooksAssistant.lumbridgeWheatField.contains(localPlayer.getPosition());
     }
 
     @Override
     public int execute() {
-        Log.info("Heading to the Lumbridge chickens");
-        Movement.walkToRandomized(bonCooksAssistant.lumbridgeChickens.getCenter());
+        Log.info("Heading to the Lumbridge wheat field");
+        Movement.walkToRandomized(bonCooksAssistant.lumbridgeWheatField.getCenter());
 
         return Random.nextInt(1500,3000);
     }
--- bonCooksAssistant/src/com/bonfire/bonCooksAssistant.java
+++ bonCooksAssistant/src/com/bonfire/bonCooksAssistant.java
@@ -1,6 +1,7 @@
 package com.bonfire;
 
 import org.rspeer.runetek.api.movement.position.Area;
+import org.rspeer.runetek.api.movement.position.Position;
 import org.rspeer.script.ScriptCategory;
 import org.rspeer.script.ScriptMeta;
 import org.rspeer.script.task.Task;
@@ -10,11 +11,17 @@ import org.rspeer.ui.Log;
 @ScriptMeta(developer = "Bonfire", name = "bonCooksAssistant", desc = "Completes the Cook's Assistant F2P quest", category = ScriptCategory.QUESTING, version = 0.11)
 public class bonCooksAssistant extends TaskScript {
 
-    private static final Task[] TASKS = {new WalkToKitchen(), new StartQuest(), new CollectStarterItems(), new WalkToCowPen(), new MilkCow(), new WalkToChickens(), new CollectEgg()};
+    private static final Task[] TASKS = {new WalkToKitchen(), new StartQuest(), new CollectStarterItems(), new WalkToCowPen(), new MilkCow(), new WalkToChickens(), new CollectEgg(), new WalkToWheatField()};
+
     static Area lumbridgeKitchen = Area.rectangular(3205, 3212, 3212, 3217);
     static Area lumbridgeCellar = Area.rectangular(3209, 9620, 3211, 9622);
     static Area lumbridgeCowPen = Area.rectangular(3256, 3270, 3253, 3273);
     static Area lumbridgeChickens = Area.rectangular(3227, 3301, 3233, 3295);
+    static Area lumbridgeWheatField = Area.polygonal(
+            new Position(3163, 3291, 0),
+            new Position(3163, 3298, 0),
+            new Position(3157, 3297, 0));
+
     static int cooksAssistantVarp = 29;
 
     @Override
